a77ad710b4d031854b54364370c041b9b781b39c
Sueltz <142297574+Sueltz@users.noreply.github.com>
2023-08-15 10:35:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.test('🥗 Kale, Spiach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spiach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
